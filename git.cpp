86d9b30069fa765741834a2e629b2d1d2f69763f
PaulOrdin <nguyenbathong9999@gmail.com>
2022-09-19 00:51:28 +0700

    Thay doi message

--- git.cpp
+++ git.cpp
@@ -2,13 +2,15 @@
 using namespace std; 
 int main()
 {
-    int a,b;
-    int sum;
+    signed int a,b;
+    signed int sum,minus;
     cout<<"Nhap vao mot so a: "<<"\n";
     cin>>a;
     cout<<"Nhap vao mot so b: "<<"\n";
     cin>>b;
     sum = a+b;
-    cout<<"Tong la:"<<"\n"<<sum<<endl;
+    cout<<"Tong la:"<<"\n"<<sum<<"\n";
+    minus = a-b;
+    cout<<"Hieu la:"<<"\n"<<minus<<endl;
     return 0;
 }
