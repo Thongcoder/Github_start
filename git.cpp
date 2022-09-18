b69d91d90d7810392f2f51cc682b1ffa60ac70cf
PaulOrdin <nguyenbathong9999@gmail.com>
2022-09-19 00:35:45 +0700

    create new message

---
+++ git.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std; 
+int main()
+{
+    int a,b;
+    int sum;
+    cout<<"Nhap vao mot so a: "<<"\n";
+    cin>>a;
+    cout<<"Nhap vao mot so b: "<<"\n";
+    cin>>b;
+    sum = a+b;
+    cout<<"Tong la:"<<"\n"<<sum<<endl;
+    return 0;
+}
